ee2f54988b3ccdfe860f4433acf46088bcc73213
adilsqe13 <adilsqe13@gmail.com>
2025-12-25 18:17:44 +0530

    reverse content

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -20,6 +20,7 @@ class ArticleController extends Controller
         return response()->json([
             'status' => true,
             'data' => [
+                'current_page' => $articles->currentPage(),
                 'per_page'     => $articles->perPage(),
                 'total'        => $articles->total(),
                 'data'         => $articles->items(),
